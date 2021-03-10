2b2547331b1da6293028c6ad34985aa3389e0f6a
nicolasances <nicolas.matteazzi@gmail.com>
2021-03-10 20:14:21 +0100

    numerical input working

--- src/comp/AmountSelector.js
+++ src/comp/AmountSelector.js
@@ -8,25 +8,53 @@ export default class AmountSelector extends Component {
         super(props);
 
         this.state = {
-            amount: 0.0
+            amount: this.props.value ? this.props.value : 0
         }
 
         this.updateAmount = this.updateAmount.bind(this);
+        this.clearValue = this.clearValue.bind(this);
 
     }
 
     updateAmount(event) {
 
+        let isNumber = /^[0-9]+((\,|\.)([0-9])*)?$/.test(event.target.value);
+        let isEmpty = event.target.value == '';
+
+        if (isNumber)
+            this.setState({
+                amount: event.target.value.replace(',', '.')
+            })
+        else if (isEmpty)
+            this.restore();
+    }
+
+    restore() {
+
         this.setState({
-            amount: event.target.value
+            amount: this.props.value ? this.props.value : 0
+        })
+    }
+
+    clearValue() {
+
+        this.setState({
+            amount: ''
         })
     }
 
     render() {
         return (
-            <div className="amount-selector" onClick={() => {document.getElementById('amount-input').focus()}}>
+            <div className="amount-selector" onClick={() => { document.getElementById('amount-input').focus() }}>
                 <div className="amount">
-                    <input id="amount-input" type="number" inputMode="decimal" defaultValue={this.state.amount.toLocaleString('it-IT')} onChange={this.updateAmount}/>
+                    <input id="amount-input"
+                        type="text"
+                        inputMode="decimal"
+                        value={this.state.amount}
+                        onChange={this.updateAmount}
+                        onFocus={this.clearValue}
+                        onBlur={this.updateAmount}
+                    />
                 </div>
             </div>
         )
