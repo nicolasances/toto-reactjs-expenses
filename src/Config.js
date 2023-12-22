01d73c95cefd3f1c35934fe447751c0fa33e3107
nicolasances <nicolas.matteazzi@gmail.com>
2023-12-22 11:03:37 +0100

    Version 1.11.0.5

--- src/Config.js
+++ src/Config.js
@@ -6,7 +6,7 @@ export const APIS = {
   games: process.env.REACT_APP_GAMES_API_ENDPOINT, 
 }
 
-export const APP_VERSION = "1.11.0.4"
+export const APP_VERSION = "1.11.0.5"
 
 export const EVENTS = {
   userInfoChanged: 'userInfoChanged', // User info received from Google
