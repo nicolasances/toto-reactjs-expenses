bfb7d2d3160c804b2941cd418caefa07fcf9f35f
nicolasances <nicolas.matteazzi@gmail.com>
2024-01-12 19:29:40 +0100

    1.15.0.1 - Investment category for incomes

--- src/services/IncomeCategoriesMap.js
+++ src/services/IncomeCategoriesMap.js
@@ -2,6 +2,7 @@ import {ReactComponent as SALARY} from '../img/incomescat/salary.svg';
 import {ReactComponent as BONUS} from '../img/incomescat/bonus.svg';
 import {ReactComponent as CHILD} from '../img/incomescat/child.svg';
 import {ReactComponent as REIMBURSEMENT} from '../img/incomescat/reimbursement.svg';
+import {ReactComponent as INVESTMENTS} from '../img/incomescat/investments.svg';
 import {ReactComponent as OTHER} from '../img/incomescat/other.svg';
 
 
@@ -10,6 +11,7 @@ export const incomeCategoriesMap = new Map([
   ['BONUS', {image: (<BONUS/>), label: 'Bonus'}],
   ['CHILD', {image: (<CHILD/>), label: 'Child support'}],
   ['REIMBURSEMENT', {image: (<REIMBURSEMENT/>), label: 'Reimbursement'}],
+  ['INVESTMENTS', {image: (<INVESTMENTS/>), label: 'Investments'}],
   ['VARIE', {image: (<OTHER/>), label: 'Other'}],
 ])
 
